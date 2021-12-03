34c08e11da7febabd4da5305c4c3aa6c6186516c
Anthony Griffith <a.griffith.gonzalez@gmail.com>
2021-12-03 16:16:43 -0600

    Header added, no funcitonality

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Searchbar = () => {
+  const [term, setTerm] = useState('');
+
   return (
     <form className="search-bar">
-      <input className="search-bar-input" />
+      <input
+        className="search-bar-input"
+        placeholder="Search movies"
+        value={term}
+        onChange={(e) => {
+          setTerm(e.target.value);
+        }}
+      />
     </form>
   );
 };
